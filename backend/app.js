20f5f7f09aae051b4c38208423956d0dc1d74649
Rohan Ramakrishnan <65002749+rohan-ramakrishnan@users.noreply.github.com>
2022-05-25 12:17:18 -0700

    fixing gitignore

--- backend/app.js
+++ backend/app.js
@@ -4,12 +4,16 @@ const bodyParser = require("body-parser");
 const app = express();
 require("dotenv").config();
 
+const indexRoutes = require("./routes/index");
+
 app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
     extended: true
   })
 );
+
+app.use("/", indexRoutes);
 
 app.listen(process.env.PORT, () => {
   console.log("Listening on port: " + process.env.PORT);
